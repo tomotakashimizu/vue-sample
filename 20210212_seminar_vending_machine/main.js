c1758cf21b02a3254e5b9c3a171b631b15d26667
tomotakashimizu <tomotaka.developer@gmail.com>
2021-02-12 22:26:39 +0900

    Vue で自動販売機を作成 localStorage を用いて保存可能

--- 20210212_seminar_vending_machine/main.js
+++ 20210212_seminar_vending_machine/main.js
@@ -14,22 +14,40 @@ new Vue({
       if (Number.isNaN(Number(this.inputValue))) {
         alert("数字を入力してください。")
         this.inputValue = ""
+      } else if (this.inputValue == "") {
+        alert("数字を入力してください。")
       } else {
         this.totalMoney += Number(this.inputValue)
+        localStorage.totalMoney = this.totalMoney
         this.inputValue = ""
       }
     },
+
     buyDrink: function (index) {
       if (this.drinks.prices[index] > this.totalMoney) {
         alert("投入金額が不足しています。")
       } else {
         this.totalMoney -= this.drinks.prices[index]
+        localStorage.totalMoney = this.totalMoney
         this.lists.push(`${this.drinks.drinks[index]}を購入しました。`)
+        localStorage.lists = JSON.stringify(this.lists)
       }
     },
+
+    clearTotalMoney: function () {
+      this.totalMoney = 0
+      localStorage.totalMoney = this.totalMoney
+    },
+
+    deleteList: function (index) {
+      this.lists.splice(index, 1)
+      localStorage.lists = JSON.stringify(this.lists)
+    },
   },
+
   // Vue インスタンスが作られるときに実行される関数 created
   created: function () {
-    // this.lists = JSON.parse(localStorage.lists)
+    this.totalMoney = localStorage.totalMoney || 0
+    this.lists = JSON.parse(localStorage.lists) || []
   },
 })
